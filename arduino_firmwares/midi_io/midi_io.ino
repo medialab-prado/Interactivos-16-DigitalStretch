3eb5a676f4e10ab815be0b9d41ba5b9c42a5ab0a
Pablo Clemente <paclema@gmail.com>
2016-06-09 17:39:57 +0200

    midi_io updated to map values from calibrated results

--- arduino_firmwares/midi_io/midi_io.ino
+++ arduino_firmwares/midi_io/midi_io.ino
@@ -1,8 +1,10 @@
 #include "MIDIUSB.h"
 
-#define NUM_STRETCH_SENSORS 3
-#define DEBUG 0
+#define NUM_STRETCH_SENSORS 2
+#define DEBUG false
 
+int reading_max[NUM_STRETCH_SENSORS+1] = { 435, 1023 };
+int reading_min[NUM_STRETCH_SENSORS+1] = { 212, 0 };
 void setup() {
   Serial.begin(115200);
 }
@@ -17,8 +19,8 @@ void loop() {
   for(int i=0; i<NUM_STRETCH_SENSORS;i++){
     int reading;
     reading = analogRead(i);
-    reading = map(reading, 0, 1023, 0, 127);
+    reading = map(reading, reading_min[i], reading_max[i], 0, 127);
     if(DEBUG) Serial.println(reading);
-    else  controlChange(0, i, reading);
+    else  controlChange(0, 1+i, reading);
   }
 }
