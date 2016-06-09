019d5721f21cd9241fecb5b468aa7070d82ba304
Pablo Clemente <paclema@gmail.com>
2016-06-09 17:28:25 +0200

    Updated sensor calibrate program

--- arduino_firmwares/sensor_calibrate/sensor_calibrate.ino
+++ arduino_firmwares/sensor_calibrate/sensor_calibrate.ino
@@ -1,8 +1,8 @@
 #define NUM_STRETCH_SENSORS 2
 
-int reading_max[NUM_STRETCH_SENSORS];
-int reading_min[NUM_STRETCH_SENSORS];
-int reading[NUM_STRETCH_SENSORS];
+int reading_max[NUM_STRETCH_SENSORS+1];
+int reading_min[NUM_STRETCH_SENSORS+1];
+int reading[NUM_STRETCH_SENSORS+1];
 
 void setup() {
   Serial.begin(115200);
@@ -16,20 +16,20 @@ void loop() {
   for(int i=0; i<NUM_STRETCH_SENSORS;i++){
     reading[i] = analogRead(i);
     if(reading[i]>=reading_max[i])  reading_max[i] = reading[i];
-    if(reading[i]<=reading_min[i])  reading_mmin[i] = reading[i];
-
-    Serial.print("Sensors min: { ");
-    Serial.print(reading_min[i]);
-    Serial.print(", ");
-    Serial.println(" };");
-
-    Serial.print("Sensors max: { ");
-    Serial.print(reading_min[i]);
-    Serial.print(", ");
-    Serial.println(" };");
+    if(reading[i]<=reading_min[i])  reading_min[i] = reading[i];
   }
-
-
-
+  
+  Serial.print("Sensors min: { ");
+  for(int i=0; i<NUM_STRETCH_SENSORS;i++){
+    Serial.print(reading_min[i]);
+    if(i!=(NUM_STRETCH_SENSORS-1))  Serial.print(", ");
+  }
+  Serial.println(" };");
+  Serial.print("Sensors max: { ");
+  for(int i=0; i<NUM_STRETCH_SENSORS;i++){
+    Serial.print(reading_max[i]);
+    if(i!=(NUM_STRETCH_SENSORS-1))  Serial.print(", ");
+  }
+  Serial.println(" };");
 
 }
